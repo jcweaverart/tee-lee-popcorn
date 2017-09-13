cb339e95e199cbace6bcdc83107b96e660e1cc05
User <user@Users-Computer-7.local>
2017-09-13 11:29:37 -0500

    added bin/www file and added a few changes to the app.js. also made sure to include bodyparser and added path to the engine template generator for cross compatibility with windows systems and linux alike. Hopefully this works with a heroku deployment now. Also, changed some scripts in the package.json file like getting rid of unneccesary start and test scripts and including an engines spec.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const path = require('path');
 const helmet = require('helmet');
 const pug = require('pug');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', "pug");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(helmet());
 app.use(compression());
-app.use(express.static('public'));
-
-app.set('view engine', "pug");
+app.use(express.static(path.join(__dirname,'public')));
 
 const mainRoute = require('./routes/index');
 const subRoutes = require('./routes/subpages');
@@ -17,4 +21,15 @@ const subRoutes = require('./routes/subpages');
 app.use('/', mainRoute);
 app.use('/sub', subRoutes);
 
-app.listen(3000);
+app.use((req, res, next) => {
+    const err = new Error("Page Not found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.staus || 500);
+    res.render("error");
+});
+
+module.exports= app;
