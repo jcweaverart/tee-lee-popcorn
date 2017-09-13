2f149294388097b102f96a127e29d964905cb2f5
User <user@Users-Computer-7.local>
2017-09-13 11:41:53 -0500

    screwing things up for sure but I will make them work again. the last iteration worked totally well, however, I want to know more about what went wrong the first time so I took out some of the possible culprits to see.

--- app.js
+++ app.js
@@ -32,4 +32,24 @@ app.use((err, req, res, next) => {
     res.render("error");
 });
 
-module.exports= app;
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+}
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+app.listen(port);
